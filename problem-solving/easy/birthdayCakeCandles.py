b5bd10311e69007279bbab3bdf324352ab338068
Roshan Sharma <40719199+roshnet@users.noreply.github.com>
2018-09-20 19:12:26 +0530

    Create birthdayCakeCandles.py
    URL for this problem:
    https://www.hackerrank.com/challenges/birthday-cake-candles/problem

---
+++ problem-solving/easy/birthdayCakeCandles.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the birthdayCakeCandles function below.
+def birthdayCakeCandles(ar):
+    
+    n = len(ar)
+    tallest = max(ar)
+    occurs = 0
+    for i in range(n):
+        if tallest == ar[i]:
+            occurs = occurs + 1
+    return occurs
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
